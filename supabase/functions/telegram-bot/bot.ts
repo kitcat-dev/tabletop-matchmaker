73128c1af2f451839d3bdf0e7fb19902ca4aa5b5
Albert Abdulmanov <albert.abdu@gmail.com>
2024-03-07 15:53:32 +0100

    fallback

--- supabase/functions/telegram-bot/bot.ts
+++ supabase/functions/telegram-bot/bot.ts
@@ -10,25 +10,26 @@ import {
 	type ChatMembersFlavor,
 } from "https://deno.land/x/grammy_chat_members@v1.0.12/mod.ts";
 
-type MyContext = Context & ChatMembersFlavor;
-const adapter = new MemorySessionStorage<ChatMember>();
+// const adapter = new MemorySessionStorage<ChatMember>();
 
 const botToken = Deno.env.get("TELEGRAM_BOT_TOKEN");
-
 assert(botToken);
 
-export const bot = new Bot<MyContext>(botToken);
+export const bot = new Bot<Context & ChatMembersFlavor>(botToken);
 
-bot.use(chatMembers(adapter));
-bot.start({
-	allowed_updates: ["chat_member", "message"],
-});
-bot.on("message", async (ctx) => {
-	const chatMember = await ctx.chatMembers.getChatMember();
+// bot.use(chatMembers(adapter));
+// bot.start({
+// 	allowed_updates: ["chat_member", "message"],
+// });
+// bot.on("message", async (ctx) => {
+// 	const chatMember = await ctx.chatMembers.getChatMember();
 
-	return ctx.reply(
-		`Hello, ${chatMember.user.first_name}! I see you are a ${chatMember.status} of this chat!`,
-	);
-});
+// 	return ctx.reply(
+// 		`Привет, ${chatMember.user.first_name} (${chatMember.status})!\n` +
+// 			`Все говорят "${ctx.message.text}", а ты слона купи.`,
+// 	);
+// });
+
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+
+bot.command("ping", (ctx) => ctx.reply(`Pong! ${Date.now()}`));
